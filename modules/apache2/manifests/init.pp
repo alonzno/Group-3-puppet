73a82a94b7ccfcec0eda1a94537c5eb27306d245
alonzno <alonzoa@cs.uoregon.edu>
2018-07-22 19:56:19 -0700

    Added file rule

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -4,10 +4,21 @@ class apache2 {
         ensure => installed,
     }
 
+    file {"/etc/apache2/apache2.conf": 
+        ensure => present,
+        mode => 644,
+        owner => root,
+        group => root,
+
+        require => Package["apache2"],
+        source => "puppet:///modules/apache2/apache2.conf",
+    }
+
     service { 'apache2':
         enable => true,
         ensure => running,
 
-        require=> [Package["apache2"]],
+        require => [Package["apache2"], File["/etc/apache2/apache2.conf"]],
+        subscribe => [Package["apache2"], File["/etc/apache2/apache2.conf"]],
     }
 }
