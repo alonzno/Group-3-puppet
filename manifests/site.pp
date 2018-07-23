bef6bccae87c9ae8c459d3c49c6f615883c27b3b
alonzno <alonzoa@cs.uoregon.edu>
2018-07-22 19:21:53 -0700

    Include httpd in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,16 @@
 node ip-10-0-5-169 {
     include sshd
+    include httpd
 }
 
 node ip-10-0-5-58 {
     include sshd
+    include httpd
 }
 
 node ip-10-0-5-189 {
     include sshd
+    include httpd
 
     cron { "puppet update":
             command => "cd /etc/puppet && git pull -q origin master",
