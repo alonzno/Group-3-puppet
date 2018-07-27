43f2879cae9f286cc46c6905fca0deb4990ce1f6
alonzno <alonzoa@cs.uoregon.edu>
2018-07-26 18:04:31 -0700

    Start user add module. Called sysusers

---
+++ modules/sysusers/manifests/init.pp
@@ -0,0 +1,152 @@
+class sysusers {
+         user { 'dbeeman':
+ 			  ensure           => 'present',
+       gid              => '500',
+       home             => '/home/dbeeman',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '500',
+     }
+     user { 'jbeder':
+ 			  ensure           => 'present',
+       gid              => '501',
+       home             => '/home/jbeder',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '501',
+     }
+     user { 'mhagel':
+ 			  ensure           => 'present',
+       gid              => '502',
+       home             => '/home/mhagel',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '502',
+     }
+     user { 'ashears':
+ 			  ensure           => 'present',
+       gid              => '503',
+       home             => '/home/ashears',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '503',
+     }
+     user { 'alegge':
+ 			  ensure           => 'present',
+       gid              => '504',
+       home             => '/home/alegge',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '504',
+     }
+     user { 'alajaji':
+ 			  ensure           => 'present',
+       gid              => '505',
+       home             => '/home/alajaji',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '505',
+     }
+     user { 'injoker':
+ 			  ensure           => 'present',
+       gid              => '506',
+       home             => '/home/injoker',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '506',
+     }
+     user { 'mantail':
+ 			  ensure           => 'present',
+       gid              => '507',
+       home             => '/home/mantail',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '507',
+     }
+     user { 'vohoanvu':
+ 			  ensure           => 'present',
+       gid              => '508',
+       home             => '/home/vohoanvu',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '508',
+     }
+     user { 'Nosler':
+ 			  ensure           => 'present',
+       gid              => '509',
+       home             => '/home/Nosler',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '509',
+     }
+     user { 'aarcher':
+ 			  ensure           => 'present',
+       gid              => '510',
+       home             => '/home/aarcher',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '510',
+     }
+     user { 'crestonw':
+ 			  ensure           => 'present',
+       gid              => '511',
+       home             => '/home/crestonw',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '511',
+     }
+     user { 'quinnmil':
+ 			  ensure           => 'present',
+       gid              => '512',
+       home             => '/home/quinnmil',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '512',
+     }
+     user { 'mhernan7':
+ 			  ensure           => 'present',
+       gid              => '513',
+       home             => '/home/mhernan7',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '513',
+     }
+     user { 'stevev':
+ 			  ensure           => 'present',
+       gid              => '514',
+       home             => '/home/stevev',
+       password         => '*',
+       password_max_age => '99999',
+       password_min_age => '0',
+       shell            => '/bin/bash',
+       uid              => '514',
+     }
+}
