ad4565aba8d257fcf787d7767a09c0b9bcd80699
alonzno <alonzoa@cs.uoregon.edu>
2018-08-15 18:04:10 -0700

    change express to ensure package

--- modules/webappstack/manifests/init.pp
+++ modules/webappstack/manifests/init.pp
@@ -3,7 +3,9 @@ class webappstack {
         "nodejs":
         ensure => installed,
     }
-    nodejs::npm {'express':
+    nodejs::npm {'express from the npm registry':
+        ensure => 'present',
+        package => 'express',
         target => '/opt/packages',
     }
 }
