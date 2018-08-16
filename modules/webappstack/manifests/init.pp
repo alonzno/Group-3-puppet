694d260cf02e105b2e2163bbeb17b87640cf4193
alonzno <alonzoa@cs.uoregon.edu>
2018-08-15 18:08:11 -0700

    change source to github

--- modules/webappstack/manifests/init.pp
+++ modules/webappstack/manifests/init.pp
@@ -3,9 +3,10 @@ class webappstack {
         "nodejs":
         ensure => installed,
     }
-    nodejs::npm {'express from the npm registry':
-        ensure => 'present',
+    nodejs::npm { 'express from GitHub':
+        ensure  => 'present',
         package => 'express',
-        target => '/opt/packages',
+        source  => 'strongloop/express',
+        target  => '/opt/packages',
     }
 }
